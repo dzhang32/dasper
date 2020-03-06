232b69f6aacad2d5d46cb843196fd92b6dd89eb7
David Zhang <david.zhang.12@ucl.ac.uk>
2020-03-06 13:45:47 +0000

    merge junctions, with unit tests

---
+++ R/merge_junc.R
@@ -0,0 +1,83 @@
+#' Merge junction data from multiple samples
+#'
+#' \code{merge_junc} merges the junction data from several samples. Uses a
+#' \code{\link[dplyr]{full_join}}, so retains all unique junctions from every
+#' sample.
+#'
+#' @param junc_paths paths to files containing junction data.
+#' @param sample_ids sample ids in the same order as \code{junc_paths}.
+#' @param load_func function to load in junctions. By default, requires STAR
+#'   formatted junctions. But this could be switched dependent on user-specific
+#'   junction data format.
+#' @param chr_to_filter chromosomes you would like to keep. By default, no
+#'   filter is applied.
+#'
+#' @return junction co-ordinates and the raw counts from each sample.
+#'
+#' @export
+merge_junc <- function(junc_paths, sample_ids, load_func = .load_STAR, chr_to_filter = NULL){
+
+  if(length(junc_paths) != length(sample_ids)){
+
+    stop("Number of junc_paths does not equal to the number of sample_ids")
+
+  }
+
+  for(i in seq_along(junc_paths)){
+
+    print(stringr::str_c(Sys.time(), " - loading junctions for ", sample_ids[i], "..."))
+
+    junc_df <- load_func(junc_paths[i], sample_ids[i])
+
+    # filter for chromsomes
+    if(!is.null(chr_to_filter)){
+
+      # catch different chromosome formats
+      if(any(chr_to_filter %in% junc_df[["chr"]]) == F){
+
+        stop("No chromosomes in chr_to_filter match those of junction data")
+
+      }
+
+      junc_df <- junc_df %>%
+        dplyr::filter(chr %in% chr_to_filter)
+
+    }
+
+    if(i == 1){
+
+      junc_df_all <- junc_df
+
+    }else{
+
+      junc_df_all <- junc_df_all %>%
+        dplyr::full_join(junc_df, by = c("chr", "start", "end"))
+
+    }
+
+  }
+
+  junc_df_all[is.na(junc_df_all)] <- 0
+
+  print(stringr::str_c(Sys.time(), " - done!"))
+
+  return(junc_df_all)
+
+}
+
+# function used to load in junctions
+# can be replaced if user juncs in a diff format
+.load_STAR <- function(junc_path, sample_id){
+
+  # omits strand info, STAR only uses intron motif to determine strand
+  # instead, will add later when annotating by reference
+  junc_df <-
+    readr::read_delim(junc_path,
+                      delim = "\t",
+                      col_names = c("chr", "start", "end", "strand", "intron_motif", "annotation", "uniq_map_read_count", "multi_map_read_count", "max_overhang"),
+                      col_types = readr::cols(chr = "c", .default = "i")) %>%
+    dplyr::select(chr:end, !!sample_id := uniq_map_read_count)
+
+  return(junc_df)
+
+}
---
+++ tests/testthat/test-merge_junc.R
@@ -0,0 +1,38 @@
+context("Loading and merging junctions")
+library(dasper)
+
+example_juncs_1 <- .load_STAR("../../data-raw/example_juncs_1.txt", sample_id = "example_1")
+
+merged_juncs <-
+  merge_junc(junc_paths = c("../../data-raw/example_juncs_1.txt",
+                            "../../data-raw/example_juncs_2.txt"),
+             sample_ids = c("eg1", "eg2"),
+             load_func = .load_STAR,
+             chr_to_filter = NULL)
+
+test_that(".load_STAR has the correct output", {
+  expect_equal(nrow(example_juncs_1), 10000)
+  expect_equal(ncol(example_juncs_1), 4)
+  expect_true(tibble::is_tibble(example_juncs_1))
+})
+
+test_that("merging juncs has correct output", {
+  expect_equal(ncol(merged_juncs), 5)
+  expect_gte(nrow(merged_juncs), 10000)
+  expect_true(tibble::is_tibble(merged_juncs))
+  expect_false(any(is.na(merged_juncs)))
+})
+
+test_that("merging juncs has catches errors", {
+
+  expect_error(merge_junc(junc_paths = c("one", "two", "three"),
+                          sample_ids = c("one", "two")),
+               "Number of junc_paths does not equal to the number of sample_ids")
+
+  expect_error(merge_junc(junc_paths = c("../../data-raw/example_juncs_1.txt",
+                                         "../../data-raw/example_juncs_2.txt"),
+                          sample_ids = c("eg1", "eg2"),
+                          chr_to_filter = "not_a_chr"),
+               "No chromosomes in chr_to_filter match those of junction data")
+
+})
