37544b49e5d90f6658c4173c52e4160c2b1ec23e
David Zhang <david.zhang.12@ucl.ac.uk>
2020-03-09 14:30:02 +0000

    updated example juncs with strand info + added metadata

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Random set of junctions from two example samples
+#'
+#' A dataset containing the example junction data for 2 samples outputted from
+#' \code{\link{merge_junc}} used for vignettes and testing.
+#'
+#' @format A data frame with 19733 rows and 5 variables: \describe{
+#'   \item{chr}{chromsome}
+#'   \item{start}{first position of intron}
+#'   \item{end}{last position of intron}
+#'   \item{strand}{strand of intron}
+#'   \item{eg1}{raw junction counts for sample "eg1"}
+#'   \item{eg2}{raw junction counts for sample "eg1"}
+#'   }
+#' @source generated using data-raw/example_juncs.R
+"example_juncs"
