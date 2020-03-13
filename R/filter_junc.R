4a41876f6f8300b13f6bea924cc6640f8b6cded2
David Zhang <david.zhang.12@ucl.ac.uk>
2020-03-13 14:11:19 +0000

    added function to filter junctions + testing

---
+++ R/filter_junc.R
@@ -0,0 +1,98 @@
+#' Filter junctions
+#'
+#' \code{filter_junc} filters out noisy junctions based on raw counts,
+#' junc_width/width of junctions, annotation of the junction and whether the
+#' junction overlaps with a set of (blacklist) regions.
+#'
+#' @inheritParams annotate_junc_ref
+#'
+#' @param raw_count dataframe with columns as samples, rows as junctions, cells
+#'   as the raw count.
+#' @param count_thresh the number of counts below which a junction will be
+#'   filtered out.
+#' @param n_samp the number of samples that have to express the junction above
+#'   the \code{count_thresh}.
+#' @param junc_width numeric vector of length = 2. The first element denoting
+#'   the lower limit of junction width and the second the upper limit. Junctions
+#'   with widths outside this range will be filtered out.
+#' @param blacklist_gr any junctions overlapping this set of regions (in a
+#'   GRanges format) will be filtered out.
+#'
+#' @return list containing the filtered set of junctions. Metadata in a GRanges
+#'   format and raw counts as a dataframe.
+#'
+#' @export
+filter_junc <- function(junc_metadata, raw_count,
+                        count_thresh = NULL, n_samp = 1, junc_width = NULL, junc_cats = NULL, blacklist_gr = NULL){
+
+  # keep in all junctions by default
+  junc_filter <- !logical(length = length(junc_metadata))
+
+  if(!is.null(count_thresh)){
+
+    count_filter <- .get_count_filter(raw_count, count_thresh, n_samp)
+    junc_filter <- junc_filter & count_filter
+
+  }
+
+  if(!is.null(junc_width)){
+
+    junc_filter <- junc_filter & (width(junc_metadata) >= junc_width[1]) & (width(junc_metadata) <= junc_width[2])
+
+  }
+
+  if(!is.null(junc_cats)){
+
+    # check that user has inputted cats that are expected
+    exp_junc_cats <- c("annotated", "novel_acceptor", "novel_donor", "novel_exon_skip", "novel_combo", "ambig_gene", "none")
+    mismatch <- junc_cats[!(junc_cats %in% exp_junc_cats)]
+
+    if(length(mismatch) != 0){
+
+      warning(stringr::str_c("The following junction categories are not expected so ignored:\n",
+                             stringr::str_c(mismatch, collapse = ", ")))
+
+    }
+
+    junc_filter <- junc_filter & !(mcols(junc_metadata)[["junc_cat"]] %in% junc_cats)
+
+  }
+
+  if(!is.null(blacklist_gr)){
+
+    hits <- findOverlaps(junc_metadata, blacklist_gr)
+    junc_filter <- junc_filter & !(1:length(junc_filter) %in% queryHits(hits))
+
+  }
+
+  juncs_filtered <- list(metadata = junc_metadata[junc_filter],
+                         raw_count = raw_count[junc_filter,])
+
+  return(juncs_filtered)
+
+}
+
+#' Filter junctions
+#'
+#' \code{get_junc_count_filter} obtains a logical vector with TRUE marking the
+#' junctions that are above the count filter specified.
+#'
+#' @inheritParams filter_junc
+#'
+#' @return logical vector with TRUE meaning the junction has passed the count
+#'   filter.
+.get_count_filter <- function(raw_count, count_thresh, n_samp){
+
+  raw_count_ab_thresh <-
+    raw_count %>%
+    apply(MARGIN = 1,
+          FUN = function(x, count_thresh) sum(x >= count_thresh),
+          count_thresh = count_thresh) %>%
+    unlist()
+
+  raw_count_filter <- (raw_count_ab_thresh >= n_samp)
+
+  return(raw_count_filter)
+
+}
+
---
+++ tests/testthat/test-filter_junc.R
@@ -0,0 +1,46 @@
+context("Testing junction filter")
+
+load("example_juncs_w_annot.rda")
+
+juncs <-
+  filter_junc(junc_metadata = example_juncs_w_annot[["metadata"]],
+              raw_count = example_juncs_w_annot[["raw_count"]],
+              count_thresh = 5,
+              n_samp = 1,
+              junc_width = c(20, 500000),
+              junc_cats = c("ambig_gene", "none"),
+              blacklist_gr = GRanges("1:1-100000000"))
+
+test_that("incorrect junction categories are picked up", {
+  expect_warning(filter_junc(junc_metadata = example_juncs_w_annot[["metadata"]],
+                             raw_count = example_juncs_w_annot[["raw_count"]],
+                             junc_cats = c("ambig_gene", "none", "check", "check2")),
+                 regexp = "The following junction categories are not expected so ignored")
+})
+
+test_that("each filter has been applied correctly", {
+
+  # first check that without filters applied
+  # there are junctions that match the filter criteria
+  count_test <-
+  example_juncs_w_annot[["raw_count"]] %>%
+    apply(MARGIN = 1, FUN = function(x) (sum(x >= 5)))
+
+  expect_true(any(count_test < 1))
+  expect_true(any(width(example_juncs_w_annot[["metadata"]]) < 20))
+  expect_true(any(width(example_juncs_w_annot[["metadata"]]) > 500000))
+  expect_true(any(example_juncs_w_annot[["metadata"]]$junc_cat %in% c("ambig_gene", "none")))
+  expect_true(length(findOverlaps(example_juncs_w_annot$metadata, GRanges("1:1-100000000"))) > 0)
+
+  # then check none are left after filtering
+  count_test <-
+  juncs[["raw_count"]] %>%
+    apply(MARGIN = 1, FUN = function(x) (sum(x >= 5)))
+
+  expect_true(all(count_test >= 1))
+  expect_false(any(width(juncs$metadata) < 20))
+  expect_false(any(width(juncs$metadata) > 1e6))
+  expect_false(any(juncs$metadata$junc_cat %in% c("ambig_gene", "none")))
+  expect_true(length(findOverlaps(juncs$metadata, GRanges("1:1-100000000"))) == 0)
+
+})
