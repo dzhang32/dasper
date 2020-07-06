280059e0a763f65a3412cfcbf921b36b8a5c3859
David Zhang <david.zhang.12@ucl.ac.uk>
2020-07-06 15:27:05 +0100

    junc_load() - update DESCRIPTION and NAMESPACE

---
+++ data-raw/example_juncs.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(stringr)
+
+juncs <- read_delim("/data/RNA_seq_diag/mito/STAR/L1556-2624F_SJ.out.tab",
+    delim = "\t",
+    col_names = F,
+    col_types = cols(X1 = "c")
+)
+
+# generate two sets of random 10,000 juncs for testing
+for (i in 1:2) {
+    set.seed(i)
+    rand_indexes <- sort(sample(1:nrow(juncs), 10000))
+    example_juncs <- juncs[rand_indexes, ]
+
+    # store these in inst/extdata for example loading in the vignette
+    # raw data should live here in according to http://r-pkgs.had.co.nz/data.html
+    write_delim(example_juncs,
+        str_c("inst/extdata/example_juncs_", i, ".txt"),
+        delim = "\t",
+        col_names = F
+    )
+}
+
+example_juncs_1_path <- system.file("extdata", "example_juncs_1.txt", package = "dasper", mustWork = TRUE)
+example_juncs_2_path <- system.file("extdata", "example_juncs_2.txt", package = "dasper", mustWork = TRUE)
+
+example_juncs <-
+    junc_load(
+        junc_paths = c(example_juncs_1_path, example_juncs_2_path),
+        chr_to_filter = c()
+    )
+
+usethis::use_data(example_juncs, compress = "gzip")
