8adf16966109d8c23b986c43cd3af480ad2e0e55
David Zhang <david.zhang.12@ucl.ac.uk>
2020-07-10 15:26:06 +0100

    add new junctions example, with GTEx controls obtained via .junction_dl_controls() for testing/vignette

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Set of example junctions from 5 samples
+#'
+#' A dataset containing the example junction data for 2 case and 3 control
+#' samples outputted from \code{\link{junction_load}}.	The junctions have been
+#' filtered for only those lying on chromosome 21 or 22.
+#'
+#' @format RangedSummarizedExperiment from \code{\link{SummarizedExperiment}}
+#'   detailing the counts, co-ordinates of junctions lying on chromosome 21/22
+#'   for 2 example samples and 3 controls: \describe{ \item{assays}{matrix with
+#'   counts for junctions (rows) and 5 samples (cols)} \item{colData}{example
+#'   sample metadata} \item{rowRanges}{\code{\link[GenomicRanges]{GRanges}}
+#'   object describing the co-ordinates and strand of each junction} }
+#'
+#' @source generated using data-raw/junctions_example.R
+"junctions_example"
---
+++ data-raw/junctions_example.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(stringr)
+
+junctions <- read_delim("/data/RNA_seq_diag/mito/STAR/L1556-2624F_SJ.out.tab",
+    delim = "\t",
+    col_names = F,
+    col_types = cols(X1 = "c")
+)
+
+# generate two sets of random 10,000 junctions for testing
+for (i in 1:2) {
+    set.seed(i)
+    rand_indexes <- sort(sample(1:nrow(junctions), 10000))
+    junctions_example <- junctions[rand_indexes, ]
+
+    # store these in inst/extdata for example loading in the vignette
+    # raw data should live here in according to http://r-pkgs.had.co.nz/data.html
+    write_delim(junctions_example,
+        str_c("inst/extdata/junctions_example_", i, ".txt"),
+        delim = "\t",
+        col_names = F
+    )
+}
+
+# generate a junctions_example.rda for testing/vignette and as illustration of expected junction format for users
+junctions_example_1_path <- system.file("extdata", "junctions_example_1.txt", package = "dasper", mustWork = TRUE)
+junctions_example_2_path <- system.file("extdata", "junctions_example_2.txt", package = "dasper", mustWork = TRUE)
+
+# only take chr21 + 22 and first 3 GTEx control samples to save space
+junctions_example <-
+  junction_load(junction_paths = c(junctions_example_1_path, junctions_example_2_path),
+                controls = "fibroblasts",
+                chrs = c("21", "22"))
+junctions_example <- junctions_example[, c(1:5)]
+
+usethis::use_data(junctions_example, compress = "gzip")
